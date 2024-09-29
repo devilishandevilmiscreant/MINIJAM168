854c5b80e3905a7b71167b063384b839705618cd
KlausKingsman <klauskingsman@gmail.com>
2024-09-28 19:08:57 -0600

    added sprites

--- Defender.cs
+++ Defender.cs
@@ -3,22 +3,22 @@ using System;
 
 public partial class Defender : Sprite2D
 {
-    //The speed at which the defender moves in pixels/second
-    [Export] public float Speed = 200;
-    Vector2 movementDirection = Vector2.Zero;
+	//The speed at which the defender moves in pixels/second
+	[Export] public float Speed = 200;
+	Vector2 movementDirection = Vector2.Zero;
 
-    public override void _Process(double delta)
-    {
-        Position += new Vector2(movementDirection.X * Speed * (float)delta, 0);
-        Position = Position.X > 900 ? new Vector2(900, Position.Y) : Position;
-        Position = Position.X < 60 ? new Vector2(60, Position.Y) : Position;
-    }
+	public override void _Process(double delta)
+	{
+		Position += new Vector2(movementDirection.X * Speed * (float)delta, 0);
+		Position = Position.X > 900 ? new Vector2(900, Position.Y) : Position;
+		Position = Position.X < 60 ? new Vector2(60, Position.Y) : Position;
+	}
  
-    public override void _Input(InputEvent @event) {
-        movementDirection = Vector2.Zero; //I declared a move_up and move_down but currently serve no purpose
-        if (Input.IsActionPressed("move_left"))
-            movementDirection.X += -1;
-        if (Input.IsActionPressed("move_right"))
-            movementDirection.X += 1;
-    }
+	public override void _Input(InputEvent @event) {
+		movementDirection = Vector2.Zero; //I declared a move_up and move_down but currently serve no purpose
+		if (Input.IsActionPressed("move_left"))
+			movementDirection.X += -1;
+		if (Input.IsActionPressed("move_right"))
+			movementDirection.X += 1;
+	}
 }
--- InvaderPopulater.cs
+++ InvaderPopulater.cs
@@ -5,43 +5,43 @@ using System.Collections.Generic;
 // [Tool]
 public partial class InvaderPopulater : Node2D
 {
-    [Export] string InvaderScene = "res://invader.tscn";
-    [Export] int NumberOfInvaders = 44;
-    [Export] int Margin = 16;
-    [Export] Vector2 Offset = new(60, 60);//40 pix gap on either side
-    // [Export] bool Update {//worthless bool, just used to call update in tool
-    //     get => true;
-    //     set => UpdateInvaders();
-    // }
-    public List<Invader> Invaders = new();
+	[Export] string InvaderScene = "res://invader.tscn";
+	[Export] int NumberOfInvaders = 44;
+	[Export] int Margin = 16;
+	[Export] Vector2 Offset = new(60, 60);//40 pix gap on either side
+	// [Export] bool Update {//worthless bool, just used to call update in tool
+	//     get => true;
+	//     set => UpdateInvaders();
+	// }
+	public List<Invader> Invaders = new();
 
-    // public void UpdateInvaders() {
-    public override void _Ready() {
-        var invaderPackedScene = GD.Load<PackedScene>(InvaderScene);
-        int invaderSize = 0;        
-        int rows = 0;
-        int widthOfRow = 0;
+	// public void UpdateInvaders() {
+	public override void _Ready() {
+		var invaderPackedScene = GD.Load<PackedScene>(InvaderScene);
+		int invaderSize = 0;        
+		int rows = 0;
+		int widthOfRow = 0;
 
-        for (int i = 0; i < NumberOfInvaders; i++) {
-            if (i != 0 && i % widthOfRow == 0 && rows % 2 == 1) {
-                GD.Print("Invador #" + i+ " omited");
-                continue; //There are gaps that are filled during the first step that we have to avoid
-            }
-            
-            Invader inv = invaderPackedScene.Instantiate<Invader>();
-            AddChild(inv);
-            
-            if (i == 0) {
-                invaderSize = inv.SpriteSize; //set sprite size on the first invader
-                widthOfRow = (960 - 2 * Margin) / (invaderSize + Margin);
-            } 
-            
-            if (Offset.X - inv.MovementDirection.X*20 + (i - widthOfRow * rows) * (invaderSize + Margin) > 900)
-                rows++;
+		for (int i = 0; i < NumberOfInvaders; i++) {
+			if (i != 0 && i % widthOfRow == 0 && rows % 2 == 1) {
+				GD.Print("Invador #" + i+ " omited");
+				continue; //There are gaps that are filled during the first step that we have to avoid
+			}
+			
+			Invader inv = invaderPackedScene.Instantiate<Invader>();
+			AddChild(inv);
+			
+			if (i == 0) {
+				invaderSize = inv.SpriteSize; //set sprite size on the first invader
+				widthOfRow = (960 - 2 * Margin) / (invaderSize + Margin);
+			} 
+			
+			if (Offset.X - inv.MovementDirection.X*20 + (i - widthOfRow * rows) * (invaderSize + Margin) > 900)
+				rows++;
 
-            inv.MovementDirection.X = 1 - (rows%2) * 2;//even rows = 1, odd rows = -1
-            inv.Position = new Vector2(Offset.X - inv.MovementDirection.X*20 + (i - widthOfRow * rows) * (invaderSize + Margin), Offset.Y + rows * (invaderSize + Margin));
-            inv.Margin = Margin;
-        }
-    }
+			inv.MovementDirection.X = 1 - (rows%2) * 2;//even rows = 1, odd rows = -1
+			inv.Position = new Vector2(Offset.X - inv.MovementDirection.X*20 + (i - widthOfRow * rows) * (invaderSize + Margin), Offset.Y + rows * (invaderSize + Margin));
+			inv.Margin = Margin;
+		}
+	}
 }
